1c858cbe447a9072529aef8c8225dfcf5b188235
Arthur Dias <arthurnascimento1111111@gmail.com>
2024-03-03 13:34:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jogo_da_Forca
+ Um jogo da forca feito com HTML, CSS e JavaScript
---
+++ assets/js/palavras.js
@@ -0,0 +1,95 @@
+// um array com palavras
+const palavras = [
+    {
+        tema: "Profissão",
+        palavra: [
+            "Pedreiro",
+            "Cozinheiro",
+            "Policial",
+            "Tecnico",
+            "Eletrecista",
+            "Desing",
+            "Jornalista",
+            "Faxineira",
+            "Engenheiro",
+            "Empredendor"
+        ]
+    },
+    {
+        tema: "Esportes",
+        palavra: [
+            "Futebol",
+            "Volei",
+            "Basquete",
+            "Ciclismo",
+            "Boxe",
+            "Atletismo",
+            "Handebol",
+            "Surfe",
+            "Judo",
+            "Ginastica"
+        ],
+    },
+    {
+        tema: "Cor",
+        palavra: [
+            "Azul",
+            "Vermelho",
+            "Rosa",
+            "Amarelo",
+            "Marrom",
+            "Cinza",
+            "Preto",
+            "Roxo",
+            "Verde",
+            "Branco"
+        ]
+    },
+    {
+        tema: "Objeto",
+        palavra: [
+            "Mesa",
+            "Cama",
+            "Cadeira",
+            "Porta",
+            "Armario",
+            "Cofre",
+            "Copo",
+            "Caixa",
+            "Tapete",
+            "Mochila"
+        ]
+    },
+    {
+        tema: "Animal",
+        palavra: [
+            "Cachorro",
+            "Tigre",
+            "Porco",
+            "Cavalo",
+            "Gato",
+            "Pato",
+            "Vaca",
+            "Boi",
+            "Viado",
+            "Javali"
+        ]
+    },
+    {
+        tema: "País",
+        palavra: [
+            "Brasil",
+            "Argetina",
+            "Chile",
+            "Equador",
+            "China",
+            "Portugal",
+            "Russia",
+            "Israel",
+            "Uruguai",
+            "Alemanha"
+        ]
+    },
+]
+
+export { palavras }
